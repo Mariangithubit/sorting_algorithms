2b3023e1ba080076a70cfb231e2f91bc098df9cb
Mariangithubit <metsyb1@hotmail.com>
2023-07-23 05:53:09 -0700

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * bubble_sort - sorts array of integers in ascending order
--- sort.h
+++ sort.h
@@ -2,7 +2,6 @@
 #define SORT_H
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <stddef.h>
 
 /**

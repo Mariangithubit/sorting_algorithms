aebb176b19102888c11f56b518bc25e332527388
Mariangithubit <metsyb1@hotmail.com>
2023-07-23 14:20:51 -0700

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,4 +8,23 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	int temp;
+	size_t i, step, min_idx;
 
+	for (step = 0; step < size - 1; step++)
+	{
+		min_idx = step;
+		for (i = step + 1; i < size; i++)
+		{
+			if (array[i] < array[min_idx])
+				min_idx = i;
+		}
+		if (min_idx != step)
+		{
+			temp = array[step];
+			array[step] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
+}
